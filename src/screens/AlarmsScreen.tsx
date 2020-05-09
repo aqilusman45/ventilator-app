057f007ea93fa66f0f724a026df1b0ed0dc02d50
aqilusman45 <aqilusman45@gmail.com>
2020-05-09 20:32:50 +0500

    Update alarms screen with new components
    Introduce preset value for ventilator configuration for
    to DetailedAlarmMetricDisplay component, Type checking condition
    for removing metrics that don't qualify for alarms screen
    
    Resolves: #20

--- src/screens/AlarmsScreen.tsx
+++ src/screens/AlarmsScreen.tsx
@@ -2,28 +2,45 @@ import * as React from 'react';
 import { View, StyleSheet, ScrollView } from 'react-native';
 import DetailedAlarmMetricDisplay from '../components/DetailedAlarmMetricDisplay';
 import { convertArrayToMatrix } from '../utils/helpers';
-import { alarmsMetrics } from '../../sample-data/data';
-import { Parameter } from '../Interfaces/Parameter';
+// import { alarmsMetrics } from '../../sample-data/data';
 import { Row } from '../components/Globals/Row';
+import initalVentilatorConfiguration from '../constants/InitialVentilatorConfiguration';
+import VentilatorConfiguration from 'src/interfaces/VentilatorConfiguration';
+
 export default function AlarmsScreen() {
-  const metrics = convertArrayToMatrix<Parameter>(alarmsMetrics, 4);
+  const metrics = convertArrayToMatrix<VentilatorConfiguration>(
+    Object.values(initalVentilatorConfiguration).filter(
+      (item) => item.name !== undefined,
+    ),
+    4,
+  );
 
   return (
     <View style={styles.gaugeContainer}>
-      <ScrollView>
+      <ScrollView
+        style={{
+          flexGrow: 1,
+        }}>
         {metrics.map((row) => {
           return (
             <Row>
               {row.map((metricToDisplay) => {
-                return (
-                  <DetailedAlarmMetricDisplay
-                    title={metricToDisplay.title}
-                    value={metricToDisplay.value}
-                    unit={metricToDisplay.unit}
-                    lowerLimit={metricToDisplay.lowerLimit}
-                    upperLimit={metricToDisplay.upperLimit}
-                  />
-                );
+                console.log(metricToDisplay);
+                // check if type is SetParameter
+                if (metricToDisplay.name) {
+                  return (
+                    <DetailedAlarmMetricDisplay
+                      title={metricToDisplay.name}
+                      value={metricToDisplay.value}
+                      unit={metricToDisplay.unit}
+                      setValue={metricToDisplay.setValue}
+                      lowerLimit={metricToDisplay.lowerLimit}
+                      upperLimit={metricToDisplay.upperLimit}
+                    />
+                  );
+                }
+
+                return null;
               })}
             </Row>
           );
@@ -36,6 +53,10 @@ export default function AlarmsScreen() {
 const styles = StyleSheet.create({
   gaugeContainer: {
     marginBottom: 15,
+    marginTop: 15,
+    flex: 1,
+    justifyContent: 'center',
     width: '100%',
+    alignSelf: 'center',
   },
 });
