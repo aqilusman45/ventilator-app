2b8be60a63eabf508d2ccb8d5b03b04cac969707
aqilusman45 <aqilusman45@gmail.com>
2020-07-12 17:39:08 +0500

    define initial vent configs state in contants

--- src/screens/ConfigurationScreen.tsx
+++ src/screens/ConfigurationScreen.tsx
@@ -6,21 +6,11 @@ import VentSwitch from '../components/VentSwitch';
 import ConfigSelect from '../components/ConfigSelect';
 import configs from '../constants/Configurables';
 import ToggleUpdate from '../components/UpdateButton';
-
-const INITIAL_STATE = {
-  'Ventilation Mode': 'PCV',
-  'I:E Ratio': '1:1',
-  'Respiratory Rate': 12,
-  'Tidal Volume': 200,
-  'Pressure': 15,
-  'Flow Trigger': 1,
-  'PEEP': 0,
-  'FiO2': '20 - 30',
-}
+import initalVentilatorConfiguration from '../constants/InitialVentilatorConfiguration';
 
 export default function LinksScreen() {
   const [canUpdate, setCanUpdate] = useState(false);
-  const [ventConfigs, setVentConfigs] = useState({...INITIAL_STATE});
+  const [ventConfigs, setVentConfigs] = useState({...initalVentilatorConfiguration});
   const [isVentilating, setIsVentilating] = useState(false);
 
   const handleState = (key: string, value: string | boolean) => {
